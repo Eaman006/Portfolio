d083865aa03e09cef967c6f119d5d494d2c4ddd8
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-08 17:00:49 +0530

    building started

---
+++ app/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-3 flex justify-between'>
+      <div className='font-bold text-3xl flex items-center'>
+        <div>Eaman|</div>
+        <div className='text-xl text-center pt-1 text-gray-400'>Computer Engineer</div>
+      </div>
+      <div className='flex gap-5'>
+        <div>Home</div>
+        <div>Services</div>
+        <div>About</div>
+        <div>Contact me</div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
