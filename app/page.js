b541104d41e6f677c770401c475d90987175116a
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-08 21:23:07 +0530

    home page nearly ready

--- app/page.js
+++ app/page.js
@@ -1,9 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="flex gap-5 justify-between bg-gradient-to-b from-gray-800">
+      <div className="m-5 w-2/3 pt-5">
+        <div className="bg-black p-2 mt-2 rounded-xl text-xl border-2 border-blue-400 w-fit flex">👋Hi there, I'm <div className="text-blue-400 px-2 font-bold">Eaman</div></div>
+        <div className="p-2 border-2 rounded-xl text-xl my-5 bg-black border-blue-400 w-3/4">
+          <div className="">A <span className="font-bold text-blue-400">Computer Engineer</span>. I engineer meaningful solutions, transforming complex challenges into elegant, impactful code.</div>
+          <div className="flex justify-evenly my-3">
+            <div className="my-2 bg-blue-500 rounded-full p-2">
+              <Link href="/">Know more</Link>  
+            </div>
+            <div className="my-2 bg-blue-500 rounded-full p-2">
+              <Link href="/">Contact me</Link>
+            </div>
+                      
+          </div>
+        </div>
+      </div>
+      <div className="w-[90vw] h-[90vh] overflow-hidden relative m-2 p-2">
+        <Image
+          src="/main.png"
+          alt="Full Screen Image"
+          fill
+          className="object-contain"
+          priority
+        />
+      </div>
     </div>
   );
 }
