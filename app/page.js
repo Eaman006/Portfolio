0ff3b4071df446df8381e656d24ce49e35fda128
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-08 21:33:07 +0530

    corrected

--- app/page.js
+++ app/page.js
@@ -6,9 +6,10 @@ export default function Home() {
     <div className="flex gap-5 justify-between bg-gradient-to-b from-gray-800">
       <div className="m-5 w-2/3 pt-5">
         <div className="bg-black p-2 mt-2 rounded-xl text-xl border-2 border-blue-400 w-fit flex">
-          <span>👋Hi there, I'm </span>
+          <span>👋Hi there, I&apos;m </span>
           <span className="text-blue-400 px-2 font-bold">Eaman</span>
-        </div>        <div className="p-2 border-2 rounded-xl text-xl my-5 bg-black border-blue-400 w-3/4">
+        </div>        
+        <div className="p-2 border-2 rounded-xl text-xl my-5 bg-black border-blue-400 w-3/4">
           <div className="">A <span className="font-bold text-blue-400">Computer Engineer</span>. I engineer meaningful solutions, transforming complex challenges into elegant, impactful code.</div>
           <div className="flex justify-evenly my-3">
             <div className="my-2 bg-blue-500 rounded-full p-2">
