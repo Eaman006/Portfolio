0b02c0073eda8fc072289bf27385c02c7a3e8389
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-12 17:59:35 +0530

    navbar update

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -1,6 +1,13 @@
+"use client"
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const getActiveClass =(path)=>{
+    return pathname === path? 'text-blue-500':'hover:text-blue-500';
+  }
   return (
     <div className='p-3 flex justify-between'>
       <div className='font-bold text-3xl flex items-center'>
@@ -8,10 +15,10 @@ const Navbar = () => {
         <div className='text-xl text-center pt-1 text-blue-400'>Computer Engineer</div>
       </div>
       <div className='flex gap-5'>
-        <div>Home</div>
-        <div>Services</div>
-        <div>About</div>
-        <div>Contact me</div>
+        <Link href="/" className={`${getActiveClass("/")}`}>Home</Link>
+        <Link href="/services" className={`${getActiveClass("/services")}`}>Services</Link>
+        <Link href="/about" className={`${getActiveClass("/about")}`}>About</Link>
+        <Link href="/contact" className={`${getActiveClass("/contact")}`}>Contact me</Link>
         
       </div>
 
