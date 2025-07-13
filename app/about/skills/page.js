aab54ffd4609ed457e03dccdd557f24ac3122257
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-13 17:00:49 +0530

    update page components with background styles and content; add new icon and remove favicon

--- app/about/skills/page.js
+++ app/about/skills/page.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className='bg-gradient-to-b from-gray-800 w-full p-4'>
+      <div className=' text-2xl text-sky-400 font-bold'>Skills</div>
+      <div className='m-2 p-2 font-bold text-xl'>Technical Skills</div>
       
     </div>
   )
