2314d9bc1dd5b47f73e5706ee6b667b7f3bcaa3c
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-15 19:08:51 +0530

    refactor skills section to enhance layout and add skill labels

--- app/about/skills/page.js
+++ app/about/skills/page.js
@@ -5,31 +5,70 @@ const page = () => {
   return (
     <div className='bg-gradient-to-b from-gray-800 w-full p-4'>
       <div className=' text-2xl text-sky-400 font-bold'>Skills</div>
-      <div className='m-2 p-2 font-bold text-xl'>Technical Skills</div>
-      <div className='m-2 p-2'>
-        <Image src="/python.png" width={50} height={50} alt='python'></Image>                
+      <div className='flex'>
+        <div>
+          <div className='m-2 p-2 font-bold text-xl'>Technical Skills</div>
+          <div className='flex gap-5 m-4'>
+            <div className=''>
+              <Image src="/python.png" width={50} height={50} alt='python'></Image>
+              <div>Python</div>
+            </div>
+            <div className=''>
+              <Image src="/c++.png" width={50} height={50} alt='c++'></Image>
+              <div>C++</div>
+            </div>
+
+            <div className=''>
+              <Image src="/java.png" width={50} height={50} alt='java'></Image>
+              <div>Java</div>
+            </div>
+          </div>
+          <div className='flex gap-5 m-4'>
+            <div className=''>
+              <Image src="/html.png" width={50} height={50} alt='html'></Image>
+              <div>HTML</div>
+            </div>
+            <div className=''>
+              <Image src="/css.png" width={50} height={50} alt='css'></Image>
+              <div>CSS</div>
+            </div>
+            <div className=''>
+              <Image src="/javas.png" width={50} height={50} alt='javas'></Image>
+              <div>Javascript</div>
+            </div>
+          </div>
+          <div className='flex gap-5 m-4'>
+            <div className=''>
+              <Image src="/react.png" width={50} height={50} alt='react'></Image>
+              <div>React</div>
+            </div>
+            <div className=''>
+              <Image src="/next.jpeg" width={50} height={50} alt='next'></Image>
+              <div>Next.js</div>
+            </div>
+            <div className='m-2 p-2'>
+              <Image src="/aws.png" width={50} height={50} alt='aws'></Image>
+              <div>AWS</div>
+            </div>
+          </div>
+          <div className='flex gap-5 m-4'>
+            <div className=''>
+              <Image src="/google-cloud.png" width={50} height={50} alt='gcp'></Image>
+              <div>GCP</div>
+            </div>
+            <div className=''>
+              <Image src="/ibm.png" width={50} height={50} alt='ibm'></Image>
+              <div>IBM cloud</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className='m-2 p-2 font-bold text-xl'>Soft Skills</div>
+        </div>
       </div>
-      <div className='m-2 p-2'>
-        <Image src="/c++.png" width={50} height={50} alt='c++'></Image>                
-      </div>
-      <div className='m-2 p-2'>
-        <Image src="/java.png" width={50} height={50} alt='java'></Image>                
-      </div>
-      <div className='m-2 p-2'>
-        <Image src="/html.png" width={50} height={50} alt='html'></Image>                
-      </div>
-      <div className='m-2 p-2'>
-        <Image src="/css.png" width={50} height={50} alt='css'></Image>                
-      </div>
-      <div className='m-2 p-2'>
-        <Image src="/javas.png" width={50} height={50} alt='javas'></Image>                
-      </div><div className='m-2 p-2'>
-        <Image src="/react.png" width={50} height={50} alt='react'></Image>                
-      </div>
-      <div className='m-2 p-2'>
-        <Image src="/next.jpeg" width={50} height={50} alt='next'></Image>                
-      </div>
-      
+
+
+
     </div>
   )
 }
