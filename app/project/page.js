eaaec94672202b61d65c24e3a7f9cd672bcfee7c
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-28 22:04:45 +0530

    update project details and features for Automatic Attendance System with new image and descriptions

--- app/project/page.js
+++ app/project/page.js
@@ -73,22 +73,19 @@ const page = () => {
         </div>
         <div className='m-2 p-2 w-sm bg-[#00000073] rounded-xl flex-shrink-0'>
           <div className='m-2 p-2'>
-            <div className='font-bold text-xl'>Campus Navigator</div>
-            <div className='m-2'><Image src="/campus1.png" width={300} height={400} alt='campus'></Image></div>
+            <div className='font-bold text-xl'>Automatic Attendance System</div>
+            <div className='m-2'><Image src="/attendance.png" width={180} height={400} alt='campus'></Image></div>
             <div className='m-2 text-lg font-bold'>Features</div>
-            <ul className='m-2'>
-              <li>🔍 Search for Directions – Get the best path between buildings and rooms.</li>
-              <li>🎙️ Voice-Based Navigation – Speak your query instead of typing.</li>
-              <li>🗺️ Dynamic Floor Maps – Interactive SVG-based maps for easy navigation.</li>
-              <li>📡 REST API Integration – Fetches real-time navigation paths from the backend.</li>
+            <ul className='m-2 h-51'>
+              <li>Build Using OpenCV and face_recognition in Python</li>
+              <li>Take Attendance precisely with date and time</li>
+              
             </ul>
             <div className='flex justify-center'>
               <div className='m-2'>
-                <Link href="https://github.com/Eaman006/Campus-Navigator.git" className='bg-blue-600 p-2 flex rounded-lg'><span className='m-1'><FaGithub /></span>Github</Link>
+                <Link href="https://github.com/Eaman006/AutomaticAttendanceSystem.git" className='bg-blue-600 p-2 flex rounded-lg'><span className='m-1'><FaGithub /></span>Github</Link>
               </div>
-              <div className='m-2'>
-                <Link href="https://campus-navigator-five.vercel.app/" className='bg-red-600 p-2 flex rounded-lg'><IoGlobe className='my-1' />Website</Link>
-              </div>
+              
             </div>
 
           </div>
